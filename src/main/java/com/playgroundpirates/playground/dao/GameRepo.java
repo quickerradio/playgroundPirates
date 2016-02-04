15de9c0a034033fd7c58e679e3d7377568ad7665
Charles <cjohnson7713@email.vccs.edu>
2016-02-04 17:39:14 -0500

    added Game repo

---
+++ src/main/java/com/playgroundpirates/playground/dao/GameRepo.java
@@ -0,0 +1,52 @@
+package com.playgroundpirates.playground.dao;
+
+import com.playgroundpirates.playground.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.playgroundpirates.playground.Game;
+
+/**
+ * Created by hackathon on 2/4/2016.
+ */
+public class GameRepo {
+
+    JdbcTemplate template;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        template = new JdbcTemplate(dataSource);
+    }
+
+    public Collection<Game> getTopGames() {
+        List<Game> games = new ArrayList<>();
+        template.query(
+                "SELECT * FROM MAIN_GAMEEVENTS",
+                (rs) -> {
+                    Game.Builder builder = new Game.Builder();
+                    builder.withName(rs.getString("GAME_NAME"));
+                    builder.withCategory(rs.getString("GAME_CATEGORY"));
+                    builder.withMaker(rs.getString("GAME_MAKER"));
+                    Clob clob = rs.getClob("GAME_DESC");
+                    builder.withMaxPlayers(rs.getInt("GAME_MAXPLAYER"));
+                    builder.withDesc(clob.getSubString(1, (int) clob.length()));
+                    games.add(builder.build());
+                    Blob blob = rs.getBlob("GAME_PHOTO");
+                    //builder.withDesc(blob.getSubString(1, (int) clob.length()));
+                    games.add(builder.build());
+                }
+        );
+        return games;
+    }
+}
